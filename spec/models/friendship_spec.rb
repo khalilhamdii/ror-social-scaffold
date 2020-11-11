60f87395d5d492f0caed26876bb91062f190b8ba
Daniel Ronan <danielconnorronan@gmail.com>
2020-11-11 12:25:56 +0000

    add 6 test cases to user model test

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  let(:user_check) { Friendship.reflect_on_association(:friend).macro }
-
   it 'checks if belongs to User' do
     should belong_to(:user)
   end
